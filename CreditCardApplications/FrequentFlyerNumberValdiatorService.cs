5fcfe2d61811a42204f9be84da282c9afd36381e
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-13 02:16:20 +0300

    need for mock data

---
+++ CreditCardApplications/FrequentFlyerNumberValdiatorService.cs
@@ -0,0 +1,21 @@
+﻿namespace CreditCardApplications
+{
+    public interface IFrequentlyFlyerNumberValidator
+    {
+        bool isValid(string frequentFlyerNumber);
+        void isValid(string frequentFlyerNumber, out bool isValid);
+    }
+
+    public class FrequentFlyerNumberValdiatorService : IFrequentlyFlyerNumberValidator
+    {
+        public bool isValid(string frequentFlyerNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void isValid(string frequentFlyerNumber, out bool isValid)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
