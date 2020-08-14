b7424308667588d247add2c7ba2868a9351b6bab
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-15 01:59:26 +0300

    LinqToMocks setup

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -365,14 +365,21 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void LinqToMocks()
         {
-            var validator = new Mock<IFrequentlyFlyerNumberValidator>();
-            validator.Setup(x => x.ServiceInformation.License.LicenseKey)
-                .Returns("OK");
-            validator.Setup(x => x.isValid(It.IsAny<string>()))
-                .Returns(true);
+            // // Classic setup
+            //var validator = new Mock<IFrequentlyFlyerNumberValidator>();
+            //validator.Setup(x => x.ServiceInformation.License.LicenseKey)
+            //    .Returns("OK");
+            //validator.Setup(x => x.isValid(It.IsAny<string>()))
+            //    .Returns(true);
 
-            var sut = new CreditCardApplicationEvaluator(validator.Object);
-            var application = new CreditCardApplication {Age = 25};
+            // LinqToMocks:
+            IFrequentlyFlyerNumberValidator validator =
+                Mock.Of<IFrequentlyFlyerNumberValidator>(x =>
+                    x.ServiceInformation.License.LicenseKey == "OK" &&
+                    x.isValid(It.IsAny<string>()));
+
+            var sut = new CreditCardApplicationEvaluator(validator);
+            var application = new CreditCardApplication { Age = 25 };
 
             CreditCardApplicationDecision decision = sut.Evaluate(application);
             Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
