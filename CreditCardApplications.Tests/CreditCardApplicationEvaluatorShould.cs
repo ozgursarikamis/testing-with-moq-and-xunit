6d0a04a1356046ef1fe47668142289bfbe65d613
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-15 00:44:31 +0300

    Validate If Property Is Ever Set

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -200,5 +200,23 @@ namespace CreditCardApplications.Tests
 
             mockValidator.Verify(x => x.isValid(It.IsAny<string>()), Times.Never);
         }
+
+        [Fact]
+        public void ValidateIfPropertyIsSet()
+        {
+            var mockValidator = new Mock<IFrequentlyFlyerNumberValidator>();
+            mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey)
+                .Returns("OK");
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 99_000
+            };
+            sut.Evaluate(application);
+
+            mockValidator.VerifyGet(x => x.ServiceInformation.License.LicenseKey);
+        }
     }
 }
